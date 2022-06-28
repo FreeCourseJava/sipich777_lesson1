4d97c419b22b855dc4bafafa8909824905c33f7d
sipich777 <sipich777@gmail.com>
2022-06-28 12:35:51 +0700

    3. количество оыец

---
+++ src/main/java/homework1_3.java
@@ -0,0 +1,13 @@
+public class homework1_3 {
+
+    public static void main(String[] args) {
+
+        numberSheeps(2147483647) ;
+    }
+
+    public static void numberSheeps(int sheep) {
+        System.out.println("result is equal to: " + sheep + " sheeps") ;
+
+    }
+
+}
