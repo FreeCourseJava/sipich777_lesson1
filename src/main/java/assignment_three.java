97bfd25a18de9a5d4698fbbfae3840c3d2ab80dc
sipich777 <sipich777@gmail.com>
2022-06-28 16:47:25 +0700

    кол-во овец задание 3

---
+++ src/main/java/assignment_three.java
@@ -0,0 +1,13 @@
+public class assignment_three {
+
+    public static void main(String[] args) {
+
+        numberSheeps(2147483647) ;
+    }
+
+    public static void numberSheeps(int sheep) {
+        System.out.println("result is equal to: " + sheep + " sheeps") ;
+
+    }
+
+}
