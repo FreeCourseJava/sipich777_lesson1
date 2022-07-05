2e02122827c010d012d9c8b6bcdbc437f06c2cff
sipich777 <sipich777@gmail.com>
2022-07-05 22:54:55 +0700

    1.количество дней

---
+++ src/main/java/assignment_one.java
@@ -0,0 +1,18 @@
+public class assignment_one {
+
+    public static void main(String []args){
+
+        long numberOfDays = 365L;
+        long selectedDataTime = dataMillisec(numberOfDays);
+        System.out.println(selectedDataTime);
+
+    }
+
+    public static long dataMillisec( long numberOfDays){
+        long result;
+        long selectedDataTimeMs = numberOfDays * 1000 * 60 * 60 * 24;
+        long currentTime = System.currentTimeMillis();
+        result = currentTime + selectedDataTimeMs;
+        return result;
+    }
+}
